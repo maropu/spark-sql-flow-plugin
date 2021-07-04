8e028012161d918f55969a50e5c191e8c6d34333
Takeshi Yamamuro <yamamuro@apache.org>
2021-07-04 21:16:49 +0900

    Add TPCDS-relaetd tests

---
+++ src/test/scala/org/apache/spark/sql/TPCDSFlowTestSuite.scala
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql
+
+import org.apache.spark.sql.catalyst.TableIdentifier
+
+class TPCDSFlowTestSuite extends SQLFlowTestSuite with TPCDSSchema {
+
+  private val tableNames: Iterable[String] = tableColumns.keys
+
+  private def createTable(spark: SparkSession, tableName: String): Unit = {
+    spark.sql(
+      s"""
+         |CREATE TABLE `$tableName` (${tableColumns(tableName)})
+         |USING parquet
+       """.stripMargin)
+  }
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    tableNames.foreach { tableName =>
+      createTable(spark, tableName)
+    }
+  }
+
+  override def afterAll(): Unit = {
+    tableNames.foreach { tableName =>
+      spark.sessionState.catalog.dropTable(TableIdentifier(tableName), true, true)
+    }
+    super.afterAll()
+  }
+
+  override protected def baseResourcePath = {
+    getWorkspaceFilePath("src", "test", "resources", "tpcds-flow-tests").toFile
+  }
+
+  override protected def ignoreList: Set[String] = Set(
+    // TODO: Cannot generate a dot file for `q28.sql`
+    "q28.sql",
+    // TODO: Temporarily ignored because the test fails in GitHub Actions
+    "q47.sql",
+    "q49.sql",
+    "q78.sql"
+  )
+
+  override protected def runQuery(query: String, session: SparkSession): Unit = {
+    session.sql(query).createOrReplaceTempView("v")
+  }
+}
